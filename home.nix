bc24e2912d727bdd9862869fe2f3295b1698095e
Ethan Anthony <ethan.anthony@du.edu>
2024-09-03 02:23:44 -0600

    added basic config for zsh and bash

--- home.nix
+++ home.nix
@@ -61,22 +61,16 @@ in
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ea/etc/profile.d/hm-session-vars.sh
-  #
+  programs.bash = {
+    enable = true;
+    shellAliases = myAliases;
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = myAliases;
+  };
+
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
@@ -87,6 +81,5 @@ in
     TEMPLATES = "$CURRQUARTER/xlatex/templates";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
