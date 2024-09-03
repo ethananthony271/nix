3b29433744f37819e6aafb7d2353c243e1a656af
Ethan Anthony <ethan.anthony@du.edu>
2024-09-03 11:50:45 -0600

    added dependencies for scripts

--- config/scripts/scripts.nix
+++ config/scripts/scripts.nix
@@ -3,16 +3,27 @@ let
   courseInfo = pkgs.writeShellApplication {
     name = "courseInfo";
     runtimeInputs = [
-      pkgs.jq
+      jq
+      sed
     ];
     text = builtins.readFile ./courseInfo;
   };
   courseTools = pkgs.writeShellApplication {
     name = "courseTools";
     runtimeInputs = [
-      pkgs.jq
+      jq
+      sed
     ];
     text = builtins.readFile ./courseTools;
+  };
+  importMusic = pkgs.writeShellApplication {
+    name = "importMusic";
+    runtimeInputs = with pkgs; [
+      yt-dlp
+      beet
+      sed
+    ];
+    text = builtins.readFile ./importMusic;
   };
 in {
   home.packages = [
