b31e0df880baac5329c24350fcd855645d4c5510
Ethan Anthony <ethan.anthony@du.edu>
2024-09-03 01:41:17 -0600

    added home manager

--- flake.nix
+++ flake.nix
@@ -4,17 +4,27 @@
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-24.05";
+      home-manager.url = "github:nix-community/home-manager/release-24.05";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      system = "x86-64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       leoito = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      ea = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
