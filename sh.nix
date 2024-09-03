6a5129841ab62b63614444781c7b9b7a607a1a49
Ethan Anthony <ethan.anthony@du.edu>
2024-09-03 08:40:27 -0600

    formatting

--- sh.nix
+++ sh.nix
@@ -6,16 +6,16 @@ let
     la   = "ls --all --long --header --git"           ;
     lt   = "ls --tree --level=3"                      ;
 
-    g    = "git"                                     ;
-    ga   = "git add"                                 ;
-    gs   = "git status"                              ;
-    gd   = "git diff"                                ;
-    gc   = "git commit -m"                           ;
-    gb   = "git branch"                              ;
-    go   = "git checkout"                            ;
-    gp   = "git push"                                ;
-    gm   = "git merge"                               ;
-    gf   = "git fetch"                               ;
+    g    = "git"                                      ;
+    ga   = "git add"                                  ;
+    gs   = "git status"                               ;
+    gd   = "git diff"                                 ;
+    gc   = "git commit -m"                            ;
+    gb   = "git branch"                               ;
+    go   = "git checkout"                             ;
+    gp   = "git push"                                 ;
+    gm   = "git merge"                                ;
+    gf   = "git fetch"                                ;
     gz   = "lazygit"                                  ;
     z    = "zellij"                                   ;
     za   = "zellij attach"                            ;
