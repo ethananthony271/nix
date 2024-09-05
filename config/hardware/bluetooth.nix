bc209498fbdbcb4a6351c2103b5ee56efb6c14bf
Ethan Anthony <ethan.anthony@du.edu>
2024-09-04 22:08:51 -0600

    added experimental settings

--- config/hardware/bluetooth.nix
+++ config/hardware/bluetooth.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      Experimental = true;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     bluez
