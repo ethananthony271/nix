4dc99ae9bea7beb1481989f74fbe522e10f20002
Ethan Anthony <ethan.anthony@du.edu>
2024-09-06 14:12:05 -0600

    temp disable telescope file picker

--- config/home/sources/nvim/lua/plugins/telescope.lua
+++ config/home/sources/nvim/lua/plugins/telescope.lua
@@ -38,51 +38,51 @@ return {
       require("telescope").load_extension("ui-select")
     end
   },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim"
-    },
-    config = function()
-      local fb_actions = require "telescope._extensions.file_browser.actions"
-      require.("telescope").setup {
-        mappings = {
-          ["i"] = {
-            ["<C-a>"]  = fb_actions.create,
-            ["<S-CR>"] = fb_actions.create_from_prompt,
-            ["<C-r>"]  = fb_actions.rename,
-            ["<C-x>"]  = fb_actions.move,
-            ["<C-y>"]  = fb_actions.copy,
-            ["<C-d>"]  = fb_actions.remove,
-            ["<C-o>"]  = fb_actions.open,
-            ["<C-h>"]  = fb_actions.goto_parent_dir,
-            ["<C-H>"]  = fb_actions.goto_home_dir,
-            ["<C-L>"]  = fb_actions.goto_cwd,
-            ["<C-CR>"] = fb_actions.change_cwd,
-            ["<C-f>"]  = fb_actions.toggle_browser,
-            ["<C-.>"]  = fb_actions.toggle_hidden,
-            ["<C-s>"]  = fb_actions.toggle_all,
-            ["<bs>"]   = fb_actions.backspace,
-          },
-          ["n"] = {
-            ["a"]  = fb_actions.create,
-            ["r"]  = fb_actions.rename,
-            ["x"]  = fb_actions.move,
-            ["y"]  = fb_actions.copy,
-            ["d"]  = fb_actions.remove,
-            ["o"]  = fb_actions.open,
-            ["h"]  = fb_actions.goto_parent_dir,
-            ["H"]  = fb_actions.goto_home_dir,
-            ["L"]  = fb_actions.goto_cwd,
-            ["CR"] = fb_actions.change_cwd,
-            ["f"]  = fb_actions.toggle_browser,
-            ["."]  = fb_actions.toggle_hidden,
-            ["s"]  = fb_actions.toggle_all,
-          },
-        },
-      require("telescope").load_extension("file_browser")
-      }
-    end
-  },
+  -- {
+  --   "nvim-telescope/telescope-file-browser.nvim",
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-lua/plenary.nvim"
+  --   },
+  --   config = function()
+  --     local fb_actions = require "telescope._extensions.file_browser.actions"
+  --     require.("telescope").setup {
+  --       mappings = {
+  --         ["i"] = {
+  --           ["<C-a>"]  = fb_actions.create,
+  --           ["<S-CR>"] = fb_actions.create_from_prompt,
+  --           ["<C-r>"]  = fb_actions.rename,
+  --           ["<C-x>"]  = fb_actions.move,
+  --           ["<C-y>"]  = fb_actions.copy,
+  --           ["<C-d>"]  = fb_actions.remove,
+  --           ["<C-o>"]  = fb_actions.open,
+  --           ["<C-h>"]  = fb_actions.goto_parent_dir,
+  --           ["<C-H>"]  = fb_actions.goto_home_dir,
+  --           ["<C-L>"]  = fb_actions.goto_cwd,
+  --           ["<C-CR>"] = fb_actions.change_cwd,
+  --           ["<C-f>"]  = fb_actions.toggle_browser,
+  --           ["<C-.>"]  = fb_actions.toggle_hidden,
+  --           ["<C-s>"]  = fb_actions.toggle_all,
+  --           ["<bs>"]   = fb_actions.backspace,
+  --         },
+  --         ["n"] = {
+  --           ["a"]  = fb_actions.create,
+  --           ["r"]  = fb_actions.rename,
+  --           ["x"]  = fb_actions.move,
+  --           ["y"]  = fb_actions.copy,
+  --           ["d"]  = fb_actions.remove,
+  --           ["o"]  = fb_actions.open,
+  --           ["h"]  = fb_actions.goto_parent_dir,
+  --           ["H"]  = fb_actions.goto_home_dir,
+  --           ["L"]  = fb_actions.goto_cwd,
+  --           ["CR"] = fb_actions.change_cwd,
+  --           ["f"]  = fb_actions.toggle_browser,
+  --           ["."]  = fb_actions.toggle_hidden,
+  --           ["s"]  = fb_actions.toggle_all,
+  --         },
+  --       },
+  --     require("telescope").load_extension("file_browser")
+  --     }
+  --   end
+  -- },
 }
