2de4cbfd8a37d0d58ed320d1b5589c673bea571f
Ethan Anthony <ethan.anthony@du.edu>
2024-09-07 00:12:10 -0600

    workaround for recusive behavior not hitting the individual files

--- config/home/yazi.nix
+++ config/home/yazi.nix
@@ -6,6 +6,24 @@
       executable = false;
       recursive = true;
     };
+
+    ".config/yazi/keymap.toml" = {
+      source = ./sources/yazi/keymap.toml;
+      executable = false;
+      recursive = false;
+    };
+
+    ".config/yazi/theme.toml" = {
+      source = ./sources/yazi/theme.toml;
+      executable = false;
+      recursive = false;
+    };
+
+    ".config/yazi/yazi.toml" = {
+      source = ./sources/yazi/yazi.toml;
+      executable = false;
+      recursive = false;
+    };
   };
 
   home.packages = with pkgs; [
