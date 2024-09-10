304b051c1beb90774e8a5bdc30a07a266db40129
Ethan Anthony <ethan.anthony@du.edu>
2024-09-10 14:08:37 -0600

    switch to network manager

--- config/system/network.nix
+++ config/system/network.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }: {
-  # networking.hostName = "leoito";
-
-  # networking.networkmanager.enable = true;
-  #
-  # environement.systemPackages = [
-  #   pkgs.networkmanager
-  # ];
+  networking.networkmanager.enable = true;
+  
+  environment.systemPackages = [
+    pkgs.networkmanager
+    pkgs.networkmanagerapplet
+  ];
 
   # networking.wireless = {
   #   enable = true;
@@ -14,29 +13,29 @@
   #   extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
   # };
 
-  networking = {
-    hostName = "leoito";
-    networkmanager.enable = false;
-    wireless = {
-      enable = true;
-      userControlled.enable = true;
-      networks."ORBI30" = {
-        psk = "royallotus939";
-        priority = 1;
-      };
-      networks."Little Hotspot" = {
-        psk = "joeo1029";
-        priority = 2;
-      };
-      networks.eduroam = {
-        auth = ''
-          key_mgmt=WPA-EAP
-          eap=PWD
-          identity="ethan.anthony@du.edu"
-          password="%q{?b3V*Pp"CaG$eu{@HTf=,G"
-        '';
-      };
-      extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
-    };
-  };
+  # networking = {
+  #   hostName = "leoito";
+  #   networkmanager.enable = false;
+  #   wireless = {
+  #     enable = true;
+  #     userControlled.enable = true;
+  #     networks."ORBI30" = {
+  #       psk = "royallotus939";
+  #       priority = 1;
+  #     };
+  #     networks."Little Hotspot" = {
+  #       psk = "joeo1029";
+  #       priority = 2;
+  #     };
+  #     networks.eduroam = {
+  #       auth = ''
+  #         key_mgmt=WPA-EAP
+  #         eap=PWD
+  #         identity="ethan.anthony@du.edu"
+  #         password="%q{?b3V*Pp"CaG$eu{@HTf=,G"
+  #       '';
+  #     };
+  #     extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
+  #   };
+  # };
 }
