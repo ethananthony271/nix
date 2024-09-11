3ee1748745a49af670420ec701cedf8fe63788ed
Ethan Anthony <ethan.anthony@du.edu>
2024-09-10 20:42:25 -0600

    mimetype testing

--- config/home/mime.nix
+++ config/home/mime.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   xdg.mimeApps = {
     enable = true;
+    associations.removed = {
+      "application/pdf" = ["draw.desktop"];
+    };
     associations.added = {
       "application/pdf" = ["zathura.desktop"];
       "inode/directory" = ["yazi.desktop"];
