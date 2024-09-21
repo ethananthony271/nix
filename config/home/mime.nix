648d843df8d54cb02c4521a6d96490316685867f
Ethan Anthony <ethan.anthony@du.edu>
2024-09-21 16:02:00 -0600

    updated to use zathura
    org.pwmt.zathura.dektop

--- config/home/mime.nix
+++ config/home/mime.nix
@@ -1,16 +1,12 @@
 { pkgs, ... }: {
-  xdg.mimeApps = {
+  xdg = {
     enable = true;
-    associations.removed = {
-      "application/pdf" = ["draw.desktop"];
-    };
-    associations.added = {
-      "application/pdf" = ["zathura.desktop"];
-      "inode/directory" = ["yazi.desktop"];
-    };
-    defaultApplications = {
-      "application/pdf" = ["zathura.desktop"];
-      "inode/directory" = ["yazi.desktop"];
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = ["org.pwmt.zathura.desktop"];
+        "inode/directory" = ["yazi.desktop"];
+      };
     };
   };
 }
