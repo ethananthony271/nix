3c0858113b756d2bbcc9366d14eedaf0b75e0156
Ethan Anthony <ethan.anthony@du.edu>
2024-09-25 16:44:07 -0600

    first commit

---
+++ config/python/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My Python Flake Shells";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in {
+      devShells.${system}.python38 = pkgs.mkShell {
+        nativeBuildInputs = with pkgs;
+          let
+            devpython = pkgs.python38.withPackages
+              (packages: with packages; [ virtualenv pip setuptools wheel ]);
+          in [ devpython ];
+      };
+    };
+}
