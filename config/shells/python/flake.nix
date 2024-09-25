28f302bfc414f7dfab200e771eda6ed4bfd3874a
Ethan Anthony <ethan.anthony@du.edu>
2024-09-25 16:57:13 -0600

    moved python shell flake into shells directory

--- config/shells/python/flake.nix
+++ config/shells/python/flake.nix
@@ -10,12 +10,18 @@
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs { inherit system; };
     in {
-      devShells.${system}.python39 = pkgs.mkShell {
+      devShells.${system}.python312 = pkgs.mkShell {
         nativeBuildInputs = with pkgs;
           let
-            devpython = pkgs.python38.withPackages
-              (packages: with packages; [ virtualenv pip setuptools wheel ]);
-          in [ devpython ];
+            devpython = pkgs.python312.withPackages(
+              packages: with packages; [
+                z3-solver
+                numpy
+                wheel
+              ]);
+          in[
+            devpython
+          ];
       };
     };
 }
